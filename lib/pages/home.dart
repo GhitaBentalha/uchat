5e17cee6ded9b5a78cb49e88f4f459bf8349b982
lam6600 <153466104+GhitaBentalha@users.noreply.github.com>
2024-12-21 15:43:11 +0100

    Search timer

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,12 +1,11 @@
-import 'dart:async'; // Importer le package dart:async pour le Timer
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:uchat/pages/UserSelectionPage.dart';
 import 'chatpage.dart';
-import 'package:intl/intl.dart'; // Import the intl package for DateFormat
+import 'package:intl/intl.dart';
 
-// UserAvatar widget to display the first letter of the username
 class UserAvatar extends StatelessWidget {
   final String userName;
 
@@ -14,23 +13,22 @@ class UserAvatar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // Get the first letter of the username
     String firstLetter = userName.isNotEmpty ? userName[0].toUpperCase() : '?';
 
     return Container(
-      width: 40.0, // Width of the avatar
-      height: 40.0, // Height of the avatar
+      width: 40.0,
+      height: 40.0,
       decoration: BoxDecoration(
         shape: BoxShape.circle,
-        color: Colors.purple, // Background color of the avatar
+        color: Colors.purple,
       ),
       alignment: Alignment.center,
       child: Text(
         firstLetter,
         style: TextStyle(
-          color: Colors.white, // Text color
+          color: Colors.white,
           fontWeight: FontWeight.bold,
-          fontSize: 20.0, // Font size
+          fontSize: 20.0,
         ),
       ),
     );
@@ -48,24 +46,26 @@ class _HomePageState extends State<HomePage> {
   bool _isSearching = false;
   final TextEditingController _searchController = TextEditingController();
   List<Map<String, dynamic>> _chatHistory = [];
-  Timer? _timer; // Timer pour mettre à jour les discussions
+  List<Map<String, dynamic>> _filteredChatHistory = [];
+  Timer? _timer;
 
   @override
   void initState() {
     super.initState();
-    _fetchChatHistory(); // Fetch chat history on initialization
-    _startTimer(); // Start the timer to refresh chat history
+    _fetchChatHistory();
+    _startTimer();
   }
 
+/*
   @override
   void dispose() {
-    _timer?.cancel(); // Cancel the timer when the widget is disposed
+    _timer?.cancel();
     super.dispose();
   }
-
+*/
   void _startTimer() {
-    _timer = Timer.periodic(Duration(seconds: 1), (Timer timer) {
-      _fetchChatHistory(); // Refresh chat history every second
+    _timer = Timer.periodic(Duration(seconds: 10), (Timer timer) {
+      _fetchChatHistory();
     });
   }
 
@@ -106,8 +106,6 @@ class _HomePageState extends State<HomePage> {
           });
         }
 
-        print("Messages fetched: $messages");
-
         Map<String, Map<String, dynamic>> latestMessages = {};
         for (var message in messages) {
           String otherUserId = message['senderId'] == currentUserId
@@ -122,20 +120,17 @@ class _HomePageState extends State<HomePage> {
           if (userDoc.exists) {
             String userName = userDoc['name'] ?? 'Unknown';
 
-            // Compter uniquement les nouveaux messages reçus qui ne sont pas encore marqués comme livrés
             int newMessagesCount = (message['receiverId'] == currentUserId &&
                     !message['delivered'])
                 ? 1
                 : 0;
 
-            // Mise à jour du compteur des messages non lus si l'utilisateur a déjà une conversation avec cet autre utilisateur
             if (latestMessages.containsKey(otherUserId)) {
               newMessagesCount +=
                   (latestMessages[otherUserId]!['newMessagesCount'] ?? 0)
                       as int;
             }
 
-            // Mettre à jour le dernier message si c'est le plus récent
             if (!latestMessages.containsKey(otherUserId) ||
                 (message['timestamp'] as Timestamp)
                         .compareTo(latestMessages[otherUserId]!['timestamp']) >
@@ -145,17 +140,13 @@ class _HomePageState extends State<HomePage> {
                 'timestamp': message['timestamp'],
                 'userId': otherUserId,
                 'userName': userName,
-                'newMessagesCount':
-                    newMessagesCount, // Total des nouveaux messages
-                'delivered': message['delivered'], // État de livraison
+                'newMessagesCount': newMessagesCount,
+                'delivered': message['delivered'],
               };
             }
-          } else {
-            print("User not found for ID: $otherUserId");
           }
         }
 
-        // Convertir la carte des derniers messages en une liste et la trier par timestamp (dernier d'abord)
         setState(() {
           _chatHistory = latestMessages.entries.map((entry) {
             return {
@@ -163,21 +154,17 @@ class _HomePageState extends State<HomePage> {
               'userName': entry.value['userName'],
               'lastMessage': entry.value['lastMessage'],
               'time': entry.value['timestamp'],
-              'newMessagesCount': entry
-                  .value['newMessagesCount'], // Compte des nouveaux messages
-              'delivered': entry.value['delivered'], // État de livraison
+              'newMessagesCount': entry.value['newMessagesCount'],
+              'delivered': entry.value['delivered'],
             };
           }).toList()
-            ..sort((a, b) => (b['time'] as Timestamp)
-                .compareTo(a['time'])); // Trier par temps
+            ..sort((a, b) => (b['time'] as Timestamp).compareTo(a['time']));
+          _filteredChatHistory =
+              List.from(_chatHistory); // Initialize filtered chat history
         });
-
-        print("Chat history: $_chatHistory");
       } catch (e) {
         print("Error fetching messages: $e");
       }
-    } else {
-      print("No user logged in.");
     }
   }
 
@@ -188,30 +175,56 @@ class _HomePageState extends State<HomePage> {
     if (date.year == now.year &&
         date.month == now.month &&
         date.day == now.day) {
-      return DateFormat.jm().format(date); // "HH:mm"
+      return DateFormat.jm().format(date);
     } else if (date.year == now.year &&
         date.month == now.month &&
         date.day == now.day - 1) {
       return "Yesterday";
     } else {
-      return DateFormat('MMM dd, HH:mm').format(date); // "Jan 01, HH:mm"
+      return DateFormat('MMM dd, HH:mm').format(date);
     }
+  }
+
+  void _filterChatHistory(String query) {
+    setState(() {
+      if (query.isEmpty) {
+        _filteredChatHistory = List.from(_chatHistory); // Reset filter
+      } else {
+        _filteredChatHistory = _chatHistory.where((chat) {
+          return chat['lastMessage']
+              .toString()
+              .toLowerCase()
+              .contains(query.toLowerCase());
+        }).toList();
+      }
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('ChatUp'),
+        title: _isSearching
+            ? TextField(
+                controller: _searchController,
+                decoration: InputDecoration(
+                  hintText: 'Search...',
+                  border: InputBorder.none,
+                  hintStyle: TextStyle(color: Colors.white70),
+                ),
+                style: TextStyle(color: Colors.white),
+                onChanged: _filterChatHistory,
+              )
+            : const Text('ChatUp'),
         backgroundColor: Colors.purple,
+        leading: IconButton(
+          icon: const Icon(Icons.logout), // Logout button
+          onPressed: () async {
+            await FirebaseAuth.instance.signOut();
+            Navigator.pop(context);
+          },
+        ),
         actions: [
-          IconButton(
-            icon: const Icon(Icons.logout),
-            onPressed: () async {
-              await FirebaseAuth.instance.signOut();
-              Navigator.pop(context);
-            },
-          ),
           IconButton(
             icon: Icon(_isSearching ? Icons.close : Icons.search),
             onPressed: () {
@@ -219,66 +232,70 @@ class _HomePageState extends State<HomePage> {
                 _isSearching = !_isSearching;
                 if (!_isSearching) {
                   _searchController.clear();
+                  _filterChatHistory(''); // Reset the filtered chat history
                 }
               });
             },
           ),
         ],
       ),
-      body: _chatHistory.isEmpty
-          ? Center(child: Text("No chats found. Start chatting!"))
-          : ListView.builder(
-              itemCount: _chatHistory.length,
-              itemBuilder: (context, index) {
-                final chat = _chatHistory[index];
-                return ListTile(
-                  leading: UserAvatar(userName: chat['userName']),
-                  title: Text(chat['userName']),
-                  subtitle: Row(
-                    children: [
-                      Expanded(child: Text(chat['lastMessage'])),
-                      // Affiche la bulle rouge uniquement si c'est un message reçu et non lu
-                      if (chat['newMessagesCount'] > 0)
-                        Container(
-                          margin: const EdgeInsets.only(left: 8.0),
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 8.0, vertical: 4.0),
-                          decoration: BoxDecoration(
-                            color: Colors.red,
-                            borderRadius: BorderRadius.circular(12),
-                          ),
-                          child: Text(
-                            '${chat['newMessagesCount']}',
-                            style: const TextStyle(color: Colors.white),
-                          ),
+      body: Column(
+        children: [
+          Expanded(
+            child: _filteredChatHistory.isEmpty
+                ? Center(child: Text("No chats found. Start chatting!"))
+                : ListView.builder(
+                    itemCount: _filteredChatHistory.length,
+                    itemBuilder: (context, index) {
+                      final chat = _filteredChatHistory[index];
+                      return ListTile(
+                        leading: UserAvatar(userName: chat['userName']),
+                        title: Text(chat['userName']),
+                        subtitle: Row(
+                          children: [
+                            Expanded(child: Text(chat['lastMessage'])),
+                            if (chat['newMessagesCount'] > 0)
+                              Container(
+                                margin: const EdgeInsets.only(left: 8.0),
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 8.0, vertical: 4.0),
+                                decoration: BoxDecoration(
+                                  color: Colors.red,
+                                  borderRadius: BorderRadius.circular(12),
+                                ),
+                                child: Text(
+                                  '${chat['newMessagesCount']}',
+                                  style: const TextStyle(color: Colors.white),
+                                ),
+                              ),
+                          ],
                         ),
-                    ],
+                        trailing: Text(_formatTime(chat['time'] as Timestamp)),
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => ChatPage(
+                                userId: chat['userId'],
+                                userName: chat['userName'],
+                              ),
+                            ),
+                          ).then((value) {
+                            _fetchChatHistory();
+                          });
+                        },
+                      );
+                    },
                   ),
-                  trailing: Text(_formatTime(chat['time'] as Timestamp)),
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => ChatPage(
-                          userId: chat['userId'],
-                          userName: chat['userName'],
-                        ),
-                      ),
-                    ).then((value) {
-                      // Re-fetch chat history to update the new messages count
-                      _fetchChatHistory();
-                    });
-                  },
-                );
-              },
-            ),
+          ),
+        ],
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.push(
             context,
             MaterialPageRoute(builder: (context) => UserSelectionPage()),
           ).then((value) {
-            // Re-fetch chat history to update the new messages count
             _fetchChatHistory();
           });
         },
